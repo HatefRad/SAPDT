79e8b8c8c9caa3a970c2226eb091938e083156cb
Hatef Rad <hatef.sanaeirad@dianacorp.com>
2015-06-18 17:18:49 +0200

    Set maximum length for textbox + PW fields are shown with stars

--- WindowsFormsApplication1/Form2.Designer.cs
+++ WindowsFormsApplication1/Form2.Designer.cs
@@ -97,7 +97,9 @@
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(100, 237);
+            this.textBox3.MaxLength = 50;
             this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 5;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
@@ -105,7 +107,9 @@
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(100, 211);
+            this.textBox2.MaxLength = 50;
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 4;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
@@ -113,6 +117,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(100, 182);
+            this.textBox1.MaxLength = 50;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 3;
